c0dc10fd376bbbd1b66c73ba62091b8048c16ebd
Rafael <Syping@users.noreply.github.com>
2015-07-23 20:33:30 +0200

    IconThread patched

--- IconThread.cpp
+++ IconThread.cpp
@@ -17,73 +17,70 @@ void IconThread::run()
 {
     foreach(QString serverName, serverList)
     {
-        QString iconPath = smgr->getIconPath(serverName);
-        if (iconPath.left(6) != "ERROR_")
+        if (smgr->ServerManagerMode == ServerManager::RemoteMode)
         {
-            if (smgr->ServerManagerMode == ServerManager::RemoteMode)
+            // Working on that
+        }
+        else
+        {
+            QString iconPath = smgr->getIconPath(serverName);
+            if (iconPath.left(6) != "ERROR_" && QFile::exists(iconPath))
             {
-                // Working on that
-            }
-            else
-            {
-                if (QFile::exists(iconPath))
+                QPixmap serverPixmap = QPixmap(iconPath);
+                if (!serverPixmap.isNull())
                 {
-                    QPixmap serverPixmap = QPixmap(iconPath);
-                    if (!serverPixmap.isNull())
+                    if (serverPixmap.size() != QSize(24,24))
                     {
-                        if (serverPixmap.size() != QSize(24,24))
+                        if (serverPixmap.size().height() < serverPixmap.size().width())
                         {
-                            if (serverPixmap.size().height() < serverPixmap.size().width())
+                            QPixmap scaledWorkPixmap = serverPixmap.scaledToWidth(24,Qt::SmoothTransformation);
+                            if (scaledWorkPixmap.size() != QSize(24,24))
                             {
-                                QPixmap scaledWorkPixmap = serverPixmap.scaledToWidth(24,Qt::SmoothTransformation);
-                                if (scaledWorkPixmap.size() != QSize(24,24))
-                                {
-                                    int _mx2 = scaledWorkPixmap.size().height() - 24;
-                                    int _px2 = abs(_mx2);
-                                    double _pxd = _px2 / 2;
-                                    int _px = floor(_pxd);
+                                int _mx2 = scaledWorkPixmap.size().height() - 24;
+                                int _px2 = abs(_mx2);
+                                double _pxd = _px2 / 2;
+                                int _px = floor(_pxd);
 
-                                    QPixmap *finishWorkPixmap = new QPixmap(24,24);
-                                    finishWorkPixmap->fill(Qt::transparent);
-                                    QPainter drawPainter(finishWorkPixmap);
-                                    drawPainter.drawPixmap(0, _px, 24, scaledWorkPixmap.size().height(), scaledWorkPixmap);
-                                    serverPixmap = QPixmap(*(finishWorkPixmap));
-                                }
-                                else
-                                {
-                                    serverPixmap = scaledWorkPixmap;
-                                }
+                                QPixmap *finishWorkPixmap = new QPixmap(24,24);
+                                finishWorkPixmap->fill(Qt::transparent);
+                                QPainter drawPainter(finishWorkPixmap);
+                                drawPainter.drawPixmap(0, _px, 24, scaledWorkPixmap.size().height(), scaledWorkPixmap);
+                                serverPixmap = QPixmap(*(finishWorkPixmap));
                             }
                             else
                             {
-                                QPixmap scaledWorkPixmap = serverPixmap.scaledToHeight(24,Qt::SmoothTransformation);
-                                if (scaledWorkPixmap.size() != QSize(24,24))
-                                {
-                                    int _mx2 = scaledWorkPixmap.size().width() - 24;
-                                    int _px2 = abs(_mx2);
-                                    double _pxd = _px2 / 2;
-                                    int _px = floor(_pxd);
-
-                                    QPixmap *finishWorkPixmap = new QPixmap(24,24);
-                                    finishWorkPixmap->fill(Qt::transparent);
-                                    QPainter drawPainter(finishWorkPixmap);
-                                    drawPainter.drawPixmap(_px, 0, scaledWorkPixmap.size().width(), 24, scaledWorkPixmap);
-                                    serverPixmap = QPixmap(*(finishWorkPixmap));
-                                }
-                                else
-                                {
-                                    serverPixmap = scaledWorkPixmap;
-                                }
+                                serverPixmap = scaledWorkPixmap;
                             }
                         }
-                        QByteArray iconBytes;
-                        QBuffer iconBuffer(&iconBytes);
-                        iconBuffer.open(QIODevice::WriteOnly);
-                        QDataStream iconOut(&iconBuffer);
-                        iconOut << serverPixmap;
-                        iconBuffer.close();
-                        emit setServerIcon(serverName, iconBytes);
+                        else
+                        {
+                            QPixmap scaledWorkPixmap = serverPixmap.scaledToHeight(24,Qt::SmoothTransformation);
+                            if (scaledWorkPixmap.size() != QSize(24,24))
+                            {
+                                int _mx2 = scaledWorkPixmap.size().width() - 24;
+                                int _px2 = abs(_mx2);
+                                double _pxd = _px2 / 2;
+                                int _px = floor(_pxd);
+
+                                QPixmap *finishWorkPixmap = new QPixmap(24,24);
+                                finishWorkPixmap->fill(Qt::transparent);
+                                QPainter drawPainter(finishWorkPixmap);
+                                drawPainter.drawPixmap(_px, 0, scaledWorkPixmap.size().width(), 24, scaledWorkPixmap);
+                                serverPixmap = QPixmap(*(finishWorkPixmap));
+                            }
+                            else
+                            {
+                                serverPixmap = scaledWorkPixmap;
+                            }
+                        }
                     }
+                    QByteArray iconBytes;
+                    QBuffer iconBuffer(&iconBytes);
+                    iconBuffer.open(QIODevice::WriteOnly);
+                    QDataStream iconOut(&iconBuffer);
+                    iconOut << serverPixmap;
+                    iconBuffer.close();
+                    emit setServerIcon(serverName, iconBytes);
                 }
             }
         }
