2b52597b990faef583962a6e2ae044b619482c5d
Rafael <Syping@users.noreply.github.com>
2015-08-24 12:20:04 +0200

    InputBox order fix

--- src/servermgrlua/ServerManagerLua.cpp
+++ src/servermgrlua/ServerManagerLua.cpp
@@ -100,7 +100,6 @@ int ServerManagerLua::sm_run_background(lua_State *L)
         QProcess execProcess;
         execProcess.startDetached(execLine);
     }
-
     return 0;
 }
 
@@ -113,18 +112,18 @@ int ServerManagerLua::sm_gui_msgbox(lua_State *L)
     if (n == 0) return 0;
     if (n == 1)
     {
-        text = lua_tostring(L,1);
+        text = lua_tostring(L, 1);
     }
     if (n == 2)
     {
-        text = lua_tostring(L,1);
-        title = lua_tostring(L,2);
+        text = lua_tostring(L, 1);
+        title = lua_tostring(L, 2);
     }
     if (n >= 3)
     {
-        text = lua_tostring(L,1);
-        title = lua_tostring(L,2);
-        mode = lua_tostring(L,3);
+        text = lua_tostring(L, 1);
+        title = lua_tostring(L, 2);
+        mode = lua_tostring(L, 3);
     }
     QString smode = mode;
     QWidget *vThis = new QWidget();
@@ -139,23 +138,23 @@ int ServerManagerLua::sm_gui_inputbox(lua_State *L)
 {
     int n = lua_gettop(L);
     const char* title = "Server Manager Lua";
-    const char* label = "Input:";
+    const char* text = "Input:";
     QString inputText;
     QLineEdit::EchoMode InputBoxEchoMode = QLineEdit::Normal;
 
     if(n == 1)
     {
-        title = lua_tostring(L, 1);
+        text = lua_tostring(L, 1);
     }
     if(n == 2)
     {
-        title = lua_tostring(L, 1);
-        label = lua_tostring(L, 2);
+        text = lua_tostring(L, 1);
+        title = lua_tostring(L, 2);
     }
     if(n == 3)
     {
-        title = lua_tostring(L, 1);
-        label = lua_tostring(L, 2);
+        text = lua_tostring(L, 1);
+        title = lua_tostring(L, 2);
         inputText = QString(lua_tostring(L, 3));
     }
     if (n >= 4)
@@ -173,7 +172,7 @@ int ServerManagerLua::sm_gui_inputbox(lua_State *L)
     vThis->setWindowModality(Qt::ApplicationModal);
 
     bool ok;
-    inputText = QInputDialog::getText(vThis, QString(title), QString(label), InputBoxEchoMode, inputText, &ok);
+    inputText = QInputDialog::getText(vThis, QString(title), QString(text), InputBoxEchoMode, inputText, &ok);
 
     if (ok)
     {
